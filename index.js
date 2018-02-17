353b73b12f08a98dbe8b39d945239c683421fa02
zyxgo <me.rahmat.hidayat@gmail.com>
2018-02-17 16:48:40 +0800

    oauth step

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'hi mmto' });
-});
-const PORT = process.env.PORT || 5000 ;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('acces token ', accessToken);
+      console.log('refresh token ', refreshToken);
+      console.log('profile ', profile);
+
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 //localhost:5000
